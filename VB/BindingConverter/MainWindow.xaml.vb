384f34bc65fa5ea84f9d7a10b5ec3b55e550183a
DevExpressExampleBot <fakeEmail>
2023-01-26 22:45:02 +0400

    Source auto update [skip ci]

--- VB/BindingConverter/MainWindow.xaml.vb
+++ VB/BindingConverter/MainWindow.xaml.vb
@@ -1,34 +1,22 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 Imports DevExpress.XtraRichEdit
 
 Namespace BindingConverter
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-			AddHandler richEditControl1.Loaded, AddressOf richEditControl1_Loaded
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub richEditControl1_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			richEditControl1.LoadDocument("sample.docx", DocumentFormat.OpenXml)
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+            AddHandler Me.richEditControl1.Loaded, New RoutedEventHandler(AddressOf richEditControl1_Loaded)
+        End Sub
 
-
-	End Class
+        Private Sub richEditControl1_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Me.richEditControl1.LoadDocument("sample.docx", DocumentFormat.OpenXml)
+        End Sub
+    End Class
 End Namespace
